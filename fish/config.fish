3ec99f3c4e855c9e91bafb44d087c3fd5329b0e8
Sebastian0421 <zhutiantian0421@gmail.com>
2022-07-13 16:06:29 +0800

    fish config change display

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set -g -x PATH /usr/local/matlab/R2020a/bin/ $PATH
-set -g -x DISPLAY 192.168.1.102:0
+#set -g -x DISPLAY 192.168.1.102:0
 set -g fish_greeting
 set -g -x PATH /usr/local/matlab/R2020a/bin/glnxa64/ $PATH
 
